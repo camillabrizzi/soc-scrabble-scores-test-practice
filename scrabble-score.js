2523ff969d42dff82aba0d86517a0cc2f6018dc5
mohamedabdi <mohamed.abdi11@hotmail.co.uk>
2022-11-01 11:03:51 +0000

    Ticket 1

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,9 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+  if (word === "A") return 1;
+  if (word === "B") return 3;
+  if (word === "C") return 3;
+  if (word === "D") return 2;
+  if (word === "E") return 1;
+  if (word === "F") return 4;
+  if (word === "G") return 2;
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -2,3 +2,43 @@ import { calculateScrabbleScore } from "./scrabble-score";
 
 // List of matchers (expects) for jest:
 // https://jestjs.io/docs/using-matchers
+
+//ticket 1
+
+test("checks calculateScrabbleScore when A is passed in the number 1 is returned", () => {
+  let word = "";
+  const calculateScore = calculateScrabbleScore(word);
+  if (word === "A" || word === "E") {
+    expect(calculateScore.toBe(1));
+  } else if (word === "B" || word === "C") {
+    expect(calculateScore.toBe(3));
+  } else if (word === "D" || word === "G") {
+    expect(calculateScore.toBe(2));
+  } else if (word === "F") {
+    expect(calculateScore.toBe(4));
+  }
+});
+
+// test("checks calculateScrabbleScore when B is passed in the number 3 is returned", () => {
+//   const actual = calculateScrabbleScore("B");
+//   const expected = 3;
+//   expect(actual).toBe(expected);
+// });
+
+// test("checks calculateScrabbleScore when C is passed in the number 3 is returned", () => {
+//   const actual = calculateScrabbleScore("C");
+//   const expected = 3;
+//   expect(actual).toBe(expected);
+// });
+
+// test("checks calculateScrabbleScore when D is passed in the number 2 is returned", () => {
+//   const actual = calculateScrabbleScore("D");
+//   const expected = 2;
+//   expect(actual).toBe(expected);
+// });
+
+// test("checks calculateScrabbleScore when E is passed in the number 1 is returned", () => {
+//   const actual = calculateScrabbleScore("E");
+//   const expected = 1;
+//   expect(actual).toBe(expected);
+// });
